9b47f2a1e2e10cc8f0f107e54e2555c5ae60fa5f
Smith <nriagudubem@gmail.com>
2016-01-18 23:49:12 +0100

    mobile responsive refactoring

--- js/main.js
+++ js/main.js
@@ -1,23 +1,13 @@
 
  $(document).ready(function(){
-     $('.success').hide();
-     $('.failure').hide();
-     $('contactForm').submit(function(event){
-         event.preventDefault();
-         var formData = $(this).serialize();
-         $.post('mail.php','formData',status);
-          function status(data){
-              if(data == 'pass'){
-                  window.location.href('../Contacts.html');
-                  $('.success').show();
-              }
-              else{
-                  window.location.href('../Contacts.html');
-                  $('.failure').show();
 
-              }
-          }
+     $('#nav_menu').change(function(event){
+         if($(event.target).val != ""){
+             window.location.href=$(event.target).val();
+         }
 
      });
+
+
 
 });
