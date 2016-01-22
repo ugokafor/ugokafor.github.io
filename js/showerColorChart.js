8d4bccb1b8b591925eae4746959f25b1a467c9ef
ugokafor <chatmate57@gmail.com>
2016-01-22 06:15:15 +0100

    Update showerColorChart.js

--- js/showerColorChart.js
+++ js/showerColorChart.js
@@ -3,21 +3,21 @@
  */
 function white(){
     document["imgName"].src="media/immagini/shower tray 3.jpg";
-    document.getElementById("myLink").href = "white.php";
+    document.getElementById("myLink").href = "shower_tray_white.html";
 }
 function blue(){
     document["imgName"].src="media/immagini/shower=tray-blue.jpg";
-    document.getElementById("myLink").href = "blueBathtub.html";
+    document.getElementById("myLink").href = "shower_tray_blue.html";
 }
 
 function pink(){
     document["imgName"].src="media/immagini/shower-tray-pink.jpg";
-    document.getElementById("myLink").href = "pink.php";
+    document.getElementById("myLink").href = "shower_tray_pink.html";
 }
 
 function peach(){
     document["imgName"].src="media/immagini/shower-tray-peach.jpg";
-    document.getElementById("myLink").href = "peach.php";
+    document.getElementById("myLink").href = "shower_tray_peach.html";
 
 }
 
