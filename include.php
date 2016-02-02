49566fddc5695bb4de4cf43665acc2a289ed75c8
ugokafor <chatmate57@gmail.com>
2016-02-02 10:35:30 +0100

    php test
    php test

---
+++ include.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ugokafor
+ * Date: 2/2/2016
+ * Time: 10:21 AM
+ */
+
+function head(){
+    echo '<header>
+    this is a php script
+</header>';
+}
+
+
---
+++ phptest.php
@@ -0,0 +1,13 @@
+<?php
+include('include.php');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php head();?>
+</body>
+</html>
